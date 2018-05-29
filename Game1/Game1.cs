84ba2a7be82f858ba53c872e3fc3da820a577d98
timmykakes123 <regrut.9@buckeyemail.osu.edu>
2018-05-29 19:16:36 -0400

    removed comments

--- Game1/Game1.cs
+++ Game1/Game1.cs
@@ -6,10 +6,7 @@ using System.Collections;
 namespace Game1
 {
     
-    /// Tim was here
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
+    
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
@@ -23,12 +20,10 @@ namespace Game1
         public IPickupSprite StarSprite;
 
         ArrayList controllerList;
-        //public int totalFrames = 8; <= might be better in indiviual classes? Since each item will have its own spritesheet
+        
         public Vector2 startingLocation = new Vector2(400, 200); 
         public Vector2 currentLocation = new Vector2(400, 200); 
-        public int totalPickupFrames = 14; //if there is individual class later will change later
-        //public Vector2 startingLocation = new Vector2(400, 200); <= same here
-        //public Vector2 currentLocation = new Vector2(400, 200);  <= same here
+        public int totalPickupFrames = 14;
         public Texture2D marioTexture;
         public Texture2D PickupTexture;
 
@@ -40,36 +35,28 @@ namespace Game1
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+        
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            
             controllerList = new ArrayList();
-            //controllerList.Add(new GamePadController(this));
+            
             controllerList.Add(new KeyboardController(this));
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+       
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // LOAD ALL TEXTURES HERE
+            
             
             PickupTexture = Content.Load<Texture2D>("pickup");
             marioTexture = Content.Load<Texture2D>("mario");
 
-            // INITIALIZE ALL SPRITES HERE
+            
             marioSprite = new MarioSmallIdleRight(this);
             FireflowerSprite = new FireflowerSprite(this);
             CoinSprite = new CoinSprite(this);
@@ -79,20 +66,13 @@ namespace Game1
 
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
+        
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
+            
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        
         protected override void Update(GameTime gameTime)
         {
             foreach (IController controller in controllerList)
@@ -108,10 +88,7 @@ namespace Game1
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+       
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
