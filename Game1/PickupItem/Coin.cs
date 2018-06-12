1d4888aea447d0c6789b66eea0ddadb704959bac
Chow, Dorothy <chow.140@buckeyemail.osu.edu>
2018-06-12 14:22:40 -0400

    updated pickup class

--- Game1/PickupItem/Coin.cs
+++ Game1/PickupItem/Coin.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace Game1
 {
-    class Coin
+    public class Coin : IPickup
     {
+        private int cyclePosition = 0;
+        private int cycleLength = 16;
+
+        public static CoinSprite coinSprite;
+
+        public static CoinSprite CoinSprite { get => coinSprite; set => coinSprite = value; }
+
+        private Game1 myGame;
+        public Vector2 pickupLocation;
+
+        public Coin(Game1 game, Vector2 location)
+        {
+            CoinSprite = new CoinSprite(game, this);
+            myGame = game;
+            pickupLocation = location;
+        }
+
+        public void Draw()
+        {
+            cyclePosition++;
+            if (cyclePosition == cycleLength)
+            {
+                cyclePosition = 0;
+                CoinSprite.Draw();
+            }
+        }
+
+        public Vector2 GetCurrentLocation()
+        {
+            return pickupLocation;
+        }
+
+        public void Update()
+        {
+            CoinSprite.Update();
+        }
     }
 }
