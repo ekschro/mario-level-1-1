b0e40ba044db4d76ccc450e5eee45636c643e589
Chow, Dorothy <chow.140@buckeyemail.osu.edu>
2018-06-11 20:32:44 -0400

    add back enemy class

---
+++ Game1/Enemy/goomba/GoombaStateMachine.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    public class GoombaStateMachine : IEnemyStateMachine
+    {
+        private bool facingLeft = true;
+        private enum GoombaHealth { Normal, Stomped, Flipped };
+        private GoombaHealth health = GoombaHealth.Normal;
+
+
+        public void ChangeDirection()
+        {
+            facingLeft = !facingLeft;
+        }
+
+        public void BeStomped()
+        {
+            if (health != GoombaHealth.Stomped) 
+            {
+                health = GoombaHealth.Stomped;
+
+            }
+        }
+
+        public void BeFlipped()
+        {
+            if (health != GoombaHealth.Flipped) 
+            {
+                health = GoombaHealth.Flipped;
+
+            }
+        }
+
+        public void Update()
+        {
+            if (health == GoombaHealth.Flipped)
+            {
+                ChangeDirection();
+                health = GoombaHealth.Normal;
+            }
+        }
+    }
+}
