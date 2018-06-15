0223f96be34a13fadbe39b97618391eb33f6dc18
ekschro <schroeter.12@buckeyemail.osu.edu>
2018-06-15 09:32:29 -0400

    CollisionRespond class skeleton added.

---
+++ Game1/CollisionRespond.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    class CollisionRespond
+    {
+        public CollisionRespond()
+        {
+
+        }
+
+        public void BlockCollisonRespond()
+        {
+
+        }
+
+        public void EnemyCollisionRespond()
+        {
+
+        }
+
+        public void PickupCollisionRespond()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
