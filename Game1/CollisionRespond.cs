891cdf48a76749656c09b5c5c31d8393dd2247fc
ekschro <schroeter.12@buckeyemail.osu.edu>
2018-06-15 13:17:11 -0400

    New response skeleton.

--- Game1/CollisionRespond.cs
+++ Game1/CollisionRespond.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace Game1
 {
@@ -13,17 +14,62 @@ namespace Game1
 
         }
 
-        public void BlockCollisonRespond()
+        public void BlockCollisionRespondTop(IGameObject player, IGameObject block)
+        {
+            
+        }
+
+        public void BlockCollisionRespondBottom(IGameObject player, IGameObject block)
         {
 
         }
 
-        public void EnemyCollisionRespond()
+        public void BlockCollisionRespondRight(IGameObject player, IGameObject block)
         {
 
         }
 
-        public void PickupCollisionRespond()
+        public void BlockCollisionRespondLeft(IGameObject player, IGameObject block)
+        {
+
+        }
+
+        public void EnemyCollisionRespondTop(IGameObject player, IGameObject enemy)
+        {
+
+        }
+
+        public void EnemyCollisionRespondBottom(IGameObject player, IGameObject enemy)
+        {
+
+        }
+
+        public void EnemyCollisionRespondLeft(IGameObject player, IGameObject enemy)
+        {
+
+        }
+
+        public void EnemyCollisionRespondRight(IGameObject player, IGameObject enemy)
+        {
+
+        }
+
+        public void PickupCollisionRespondTop(IGameObject player, IGameObject pickup)
+        {
+
+        }
+
+        public void PickupCollisionRespondBottom(IGameObject player, IGameObject pickup)
+        {
+
+        }
+
+        public void PickupCollisionRespondLeft(IGameObject player, IGameObject pickup)
+        {
+
+        }
+
+        public void PickupCollisionRespondRight(IGameObject player, IGameObject pickup)
         {
 
         }
