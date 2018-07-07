2ea7733e14ee3614a18ad0826e66fa9f2c4e72cf
ekschro <ekschro@protonmail.com>
2018-07-06 20:42:49 -0400

    Added SoundWarehouse class.

---
+++ Game1/SoundWarehouse.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    class SoundWarehouse
+    {
+        public static Song theme;
+
+        public SoundWarehouse(Game1 game)
+        {
+            theme = game.Content.Load<Song>("01-main-theme-overworld");
+        }
+    }
+}
