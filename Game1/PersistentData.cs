805a7649b730e51937cc2d29c618454ee3a1690b
ekschro <ekschro@protonmail.com>
2018-07-07 14:45:10 -0400

    Added PersistentData class

---
+++ Game1/PersistentData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    class PersistentData
+    {
+        private int lives;
+        private int points;
+        private int coins;
+
+        public PersistentData()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
