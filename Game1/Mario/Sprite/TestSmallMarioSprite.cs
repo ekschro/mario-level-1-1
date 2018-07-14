2dfa58a13ccdd23ad2b43e8d259d39e9c501dba1
Chow, Dorothy <chow.140@buckeyemail.osu.edu>
2018-07-13 20:32:02 -0400

    fix for strange opening

--- Game1/Mario/Sprite/TestSmallMarioSprite.cs
+++ Game1/Mario/Sprite/TestSmallMarioSprite.cs
@@ -16,19 +16,19 @@ namespace Game1
         private int startFrame;
         private int endFrame;
         private IPlayer player;
-        private MarioSpriteUtility spriteUtility;
+        //private MarioSpriteUtility spriteUtility;
 
         public TestSmallMarioSprite(Game1 game, TestSmallMario Mario, IPlayer player)
         {
-            spriteUtility = new MarioSpriteUtility();
+            //spriteUtility = new MarioSpriteUtility();
             marioObject = Mario;
             myGame = game;
             currentFrame = startFrame;
             this.player = player;
             //startFrame = 41;
             //endFrame = 42;
-            startFrame = spriteUtility.SmallMarioRightIdleStart;
-            endFrame = spriteUtility.SmallMarioRightIdleEnd;
+            //startFrame = spriteUtility.SmallMarioRightIdleStart;
+            //endFrame = spriteUtility.SmallMarioRightIdleEnd;
         }
         public void ChangeFrame(int start, int end)
         {
