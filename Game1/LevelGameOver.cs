ae178748752d81690ba3237d80f7e0baff5cb871
Chow, Dorothy <chow.140@buckeyemail.osu.edu>
2018-07-17 22:24:11 -0400

    game over

---
+++ Game1/LevelGameOver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Game1
+{
+    public class LevelGameOver
+    {
+        private Game1 myGame;
+        public LevelGameOver(Game1 game)
+        {
+            myGame = game;
+
+        }
+        public void Draw()
+        {           
+            myGame.SpriteBatch.Begin();
+            myGame.SpriteBatch.DrawString(myGame.SpriteFont, "GAME OVER", new Vector2(150, 100), Color.White);
+            myGame.SpriteBatch.End();
+        }
+    }
+}
