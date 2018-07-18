fd07316306c1089d15bacb8e9bb1eddaa8e73050
Freeman, Will <freeman.694@osu.edu>
2018-07-17 23:32:59 -0400

    Game Over theme set

--- Game1/Game1.cs
+++ Game1/Game1.cs
@@ -92,7 +92,10 @@ namespace Game1
             CurrentLevel = new PlatformerLevel("../../../../Content/LevelInfo.csv", this, persistentData);
             pause = false;
             currentLevel.PlayerObject.Invulnerability = false;
-            MediaPlayer.Play(SoundWarehouse.main_theme);
+            if (persistentData.Lives > 1)
+                MediaPlayer.Play(SoundWarehouse.main_theme);
+            else
+                MediaPlayer.Play(SoundWarehouse.game_over_theme);
         }
 
         public void GameReset()
@@ -104,8 +107,7 @@ namespace Game1
             CurrentLevel = new PlatformerLevel("../../../../Content/LevelInfo.csv", this, persistentData);
             pause = false;
             currentLevel.PlayerObject.Invulnerability = false;
-            if(persistentData.Lives != 0)
-                MediaPlayer.Play(SoundWarehouse.main_theme);
+            MediaPlayer.Play(SoundWarehouse.main_theme);
         }
 
         public void CheckGameOver()
