0add4d7c2ce03ec88853fc7b32aed819338a4ae9
timmykakes123 <regrut.9@buckeyemail.osu.edu>
2018-07-25 13:46:32 -0400

    finished bowser fireball

--- Game1/BowserFireEnemySprite.cs
+++ Game1/BowserFireEnemySprite.cs
@@ -28,16 +28,16 @@ namespace Game1
 
         public void Draw()
         {
-            int width = TextureWarehouse.fireballs.Width / 4;
+            int width = TextureWarehouse.BowserFireballTexture.Width;
 
             int drawLocationX = (int)myGame.CurrentLevel.LevelCamera.PositionRelativeToCamera(bowser.CurrentXPos);
 
-            Rectangle sourceRectangle = new Rectangle(width * currentFrame, 0, width, TextureWarehouse.fireballs.Height);
-            Rectangle destinationRectangle = new Rectangle(drawLocationX, (int)bowser.CurrentYPos, width, TextureWarehouse.fireballs.Height);
+            Rectangle sourceRectangle = new Rectangle(width * currentFrame, 0, width, TextureWarehouse.BowserFireballTexture.Height);
+            Rectangle destinationRectangle = new Rectangle(drawLocationX, (int)bowser.CurrentYPos, width, TextureWarehouse.BowserFireballTexture.Height);
 
 
             myGame.SpriteBatch.Begin();
-            myGame.SpriteBatch.Draw(TextureWarehouse.fireballs, destinationRectangle, sourceRectangle, Color.White);
+            myGame.SpriteBatch.Draw(TextureWarehouse.BowserFireballTexture, destinationRectangle, sourceRectangle, Color.White);
             myGame.SpriteBatch.End();
         }
 
