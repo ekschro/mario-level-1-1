221124d10d740eb84794a1f972e68fed2654e1ba
timmykakes123 <regrut.9@buckeyemail.osu.edu>
2018-07-25 13:39:53 -0400

    fixed fire enemy. started bowser fireball.

---
+++ Game1/BowserFireEnemySprite.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game1
+{
+    public class BowserFireEnemySprite : IEnemySprite
+    {
+        BowserFireBall bowser;
+        Game1 myGame;
+        private int currentFrame;
+        public BowserFireEnemySprite(Game1 game, BowserFireBall enemy)
+        {
+            myGame = game;
+            bowser = enemy;
+            currentFrame = 0;
+        }
+
+        public void ChangeFrame(int start, int endFrame)
+        {
+            
+        }
+
+        public void ChangeSpriteEffects(SpriteEffects effects)
+        {
+            
+        }
+
+        public void Draw()
+        {
+            int width = TextureWarehouse.fireballs.Width / 4;
+
+            int drawLocationX = (int)myGame.CurrentLevel.LevelCamera.PositionRelativeToCamera(bowser.CurrentXPos);
+
+            Rectangle sourceRectangle = new Rectangle(width * currentFrame, 0, width, TextureWarehouse.fireballs.Height);
+            Rectangle destinationRectangle = new Rectangle(drawLocationX, (int)bowser.CurrentYPos, width, TextureWarehouse.fireballs.Height);
+
+
+            myGame.SpriteBatch.Begin();
+            myGame.SpriteBatch.Draw(TextureWarehouse.fireballs, destinationRectangle, sourceRectangle, Color.White);
+            myGame.SpriteBatch.End();
+        }
+
+        public void Update()
+        {
+            currentFrame++;
+            if (currentFrame == 5)
+            {
+                currentFrame = 0;
+            }
+        }
+    }
+}
